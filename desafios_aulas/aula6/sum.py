a1a72375088ef8088ae748c2aec8401248a15178
Lordeemma <mathkirby279@outlook.com>
2026-03-12 15:21:24 -0300

    Add .py files

---
+++ desafios_aulas/aula6/sum.py
@@ -0,0 +1,7 @@
+print('===== DESAFIO 03 =====')
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+_sum = num1 + num2
+
+print('A soma entre {} e {} resulta em {}'.format(num1, num2, _sum))
---
+++ exercicios/ex003.py
@@ -0,0 +1,7 @@
+# Exercício explorando os tipos de valores
+
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor: '))
+_sum = num1 + num2
+
+print('A soma entre {} e {} vale {}'.format(num1, num2, _sum))
