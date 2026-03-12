416f554857aeee2241e2f0e28c757b879196918d
Lordeemma <mathkirby279@outlook.com>
2026-03-12 14:37:12 -0300

    Commit .py files folder change for better organization

--- aula4/birthdate.py
+++
@@ -1,7 +0,0 @@
-print('===== DESAFIO 02 =====')
-
-day = input('Em que dia você nasceu? ')
-month = input('Em que mês você nasceu? ')
-year = input('Em que ano você nasceu? ')
-
-print('Sua data de nascimento é ', day, '/', month, '/', year)
--- aula4/welcome.py
+++
@@ -1,5 +0,0 @@
-print('===== DESAFIO 01 =====')
-
-name = input('Qual é seu nome? ')
-
-print('Bem-vindo(a), ', name, '!')
