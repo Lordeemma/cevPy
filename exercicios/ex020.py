911eaa456ab3b7c41af71e0d5ff0c4e6cac15abc
Lordeemma <mathkirby279@outlook.com>
2026-03-13 14:56:19 -0300

    Update ex020.py to use shuffle function

--- exercicios/ex020.py
+++ exercicios/ex020.py
@@ -1,10 +1,13 @@
 # Penúltimo exercício sobre módulos
 
-from random import choice
+from random import shuffle
 
 a1 = str(input('Insira o nome do primeiro aluno: '))
 a2 = str(input('Insira o nome do segundo aluno: '))
 a3 = str(input('Insira o nome do terceiro aluno: '))
 a4 = str(input('Insira o nome do quarto aluno: '))
+list = [a1, a2, a3, a4]
+shuffle(list)
 
-print('O aluno sorteado para apresentar o trabalho foi o(a) {}'.format(choice([a1, a2, a3, a4])))
+print('A ordem de alunos sorteada foi: ')
+print(list)
