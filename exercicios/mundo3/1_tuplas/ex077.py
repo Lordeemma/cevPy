90c0df1342e9019738496c50497fdad5f2b7fb47
Lordeemma <mathkirby279@outlook.com>
2026-03-18 18:07:36 -0300

    Refactor ex077.py

--- exercicios/mundo3/1_tuplas/ex077.py
+++ exercicios/mundo3/1_tuplas/ex077.py
@@ -2,28 +2,16 @@
 
 words = ('amar', 'praticar', 'estudo', 'python', 'livro', 'bloco')
 
-for index, w in enumerate(words):
+for w in words:
 
-    word = words[index].lower()
-    print('Na palavra {} temos'.format(words[index].upper()), end=' ')
+    word = w.lower()
+    print('Na palavra {} temos'.format(w.upper()), end=' ')
     
-    for vpos in range(0, len(word)):
+    for vpos in w:
 
-        vowel = 0
-
-        if word[vpos] == 'a':
-            vowel = 'a'
-        elif word[vpos] == 'e':
-            vowel = 'e'
-        elif word[vpos] == 'i':
-            vowel = 'i'
-        elif word[vpos] == 'o':
-            vowel = 'o'
-        elif word[vpos] == 'u':
-            vowel = 'u'
+        if vpos in 'aeiou':
+            print(vpos, end=' ')
         else:
             continue
-
-        print(vowel, end=' ')
 
     print('')
