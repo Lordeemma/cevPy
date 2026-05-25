a7c1766e32533ee99c2e1a2ad847b58f5c63fe8e
Lordeemma <mathkirby279@outlook.com>
2026-05-25 09:17:57 -0300

    Add KeyboardInterrupt handling to data input

--- exercicios/mundo3/6_tratamento_erros/ex115/funcs/banco.py
+++ exercicios/mundo3/6_tratamento_erros/ex115/funcs/banco.py
@@ -4,20 +4,28 @@ def criarPessoa(nome, idade, db):
     pessoa = dict()
 
     while True:
-        nome_pessoa = input(nome + '\033[0;32m').strip()
-        if nome_pessoa != '' and all(c.isalpha() or c.isspace() for c in nome_pessoa):
-            pessoa['nome'] = nome_pessoa
+        try:
+            nome_pessoa = input(nome + '\033[0;32m').strip()
+            
+            if nome_pessoa != '' and all(c.isalpha() or c.isspace() for c in nome_pessoa):
+                pessoa['nome'] = nome_pessoa
 
-            while True:
-                try:
-                    n = int(input('\033[m' + idade + '\033[0;32m'))
-                    pessoa['idade'] = n
-                    break
-                except ValueError:
-                    print('\033[1;31mERRO: Por favor, digite um número inteiro válido.\033[m')
-            break
-        else:
-            print('\033[1;31mERRO: O nome digitado não é válido.\033[m')
+                while True:
+                    try:
+                        n = int(input('\033[m' + idade + '\033[0;32m'))
+                        pessoa['idade'] = n
+                        break
+                    except ValueError:
+                        print('\033[1;31mERRO: Por favor, digite um número inteiro válido.\033[m')
+                    except KeyboardInterrupt:
+                        print('\n\033[1;31mOperação cancelada.\033[m')
+                        return
+                break
+            else:
+                print('\033[1;31mERRO: O nome digitado não é válido.\033[m')
+        except KeyboardInterrupt:
+            print('\n\033[1;31mOperação cancelada.\033[m')
+            return
 
     try:    
         with open(db, 'r', encoding='utf-8') as file:
